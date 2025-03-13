9d039af794917da27c6e0998a72d42f74adbbfca
MathieuBichet <bichetmathieu1@gmail.com>
2025-03-13 09:48:21 +0100

    instale extantion

--- create_db.py
+++ create_db.py
@@ -1,4 +1,7 @@
 from app import app, db
 
+# Add this line to suppress the FSADeprecationWarning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 with app.app_context():
     db.create_all()
